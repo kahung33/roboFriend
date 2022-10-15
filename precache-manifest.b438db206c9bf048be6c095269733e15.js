6b722f42cdccd0e4bab7a5fdd1848c1f4f155a63
雞ry牛羊雞ry牛羊 <kahung930809@gmail.com>
2022-10-15 00:49:28 -0600

    Updates

---
+++ precache-manifest.b438db206c9bf048be6c095269733e15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4db75733c6c3f0932f94",
+    "url": "/roboFriend/static/css/main.feb451bc.chunk.css"
+  },
+  {
+    "revision": "4db75733c6c3f0932f94",
+    "url": "/roboFriend/static/js/main.4db75733.chunk.js"
+  },
+  {
+    "revision": "d16f3c2ba5ec8e139d76",
+    "url": "/roboFriend/static/css/1.08d600f2.chunk.css"
+  },
+  {
+    "revision": "d16f3c2ba5ec8e139d76",
+    "url": "/roboFriend/static/js/1.d16f3c2b.chunk.js"
+  },
+  {
+    "revision": "4313bc563c680fe9c3c3",
+    "url": "/roboFriend/static/js/runtime~main.4313bc56.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriend/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d60011dcba6e914a59792f5e80720c00",
+    "url": "/roboFriend/index.html"
+  }
+];
